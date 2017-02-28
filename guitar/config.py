fb94378d0f1932d3a3928a5fa09d9a352ccd7f79
ruitian <ike960705@gmail.com>
2017-02-28 09:43:09 +0800

    session timeout

--- guitar/config.py
+++ guitar/config.py
@@ -29,7 +29,7 @@ class config:
 
     COOKIE_SECRET = base64.b64encode(uuid.uuid4().bytes + uuid.uuid4().bytes)
     SESSION_SECRET = base64.b64encode(uuid.uuid4().bytes + uuid.uuid4().bytes)
-    SESSION_TIMEOUT = 60
+    SESSION_TIMEOUT = 60 * 30
     TEMPLATE_PATH = os.path.join(os.path.dirname(__file__), 'templates')
     STATIC_PATH = os.path.join(os.path.dirname(__file__), 'static')
 

3bc86083c41473d575e61580d0248c095042b643
baron <baron@barons-MacBook-Pro.local>
2017-05-16 23:06:58 +0800

    alembric

---
+++ guitar/models/alembic/versions/2255c86dd660_create_account_table.py
@@ -0,0 +1,24 @@
+"""create account table
+
+Revision ID: 2255c86dd660
+Revises: 
+Create Date: 2017-05-16 18:36:27.544518
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '2255c86dd660'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
